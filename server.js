4435f27abf01b58ddc87a2a1bd9c3cf36088d0d4
ajitesh <ajitesh314mishra@gmail.com>
2022-12-25 17:00:00 +0530

    cors allow all url 3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const BookingRouter = require('./Routes/Booking')
 
 const app = express();
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:3000',
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 app.use(express.json());
